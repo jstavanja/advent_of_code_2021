50c49dd9ca95721cb6b11fa712a4da29d0c29041
Jaka Stavanja <jaka.stavanja@oryxgaming.com>
2021-12-01 21:46:44 +0100

    :tada: feat: day 1 - part 2

--- day1.ts
+++ day1.ts
@@ -1,24 +1,59 @@
 import fs from 'fs'
 
-let input: string;
-try {
-	const input = fs.readFileSync('./inputs/day1/1.txt', 'utf8')
-	let measurements: number[] = input.split(/\n/).map((measurement: string) => parseInt(measurement))
-
-	let previous = measurements[0]
-
-	measurements = measurements.slice(1)
-
-	let timesIncreased = 0
-	for (const measurement of measurements) {
-		if (measurement > previous) {
-			timesIncreased++;
-		}
-
-		previous = measurement
-	}
-
-	console.log(timesIncreased)
-} catch (err) {
-  console.error(err)
+const sum = (array: number[]) => {
+  return array.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue
+  }, 0)
 }
+
+const getEarliestWindowSumAndRemainingMeasurements = (
+  windowSize: number,
+  remainingMeasurements: number[]
+) => {
+  return {
+    sum: sum(remainingMeasurements.slice(0, windowSize)),
+    remainingMeasurements: remainingMeasurements.slice(1),
+  }
+}
+
+const getNumberOfIncreasesForWindowSize = (
+  measurements: number[],
+  windowSize: number
+) => {
+  let { sum: previousWindowSum, remainingMeasurements } =
+    getEarliestWindowSumAndRemainingMeasurements(windowSize, measurements)
+  let timesIncreased = 0
+
+  for (let i = 0; i < measurements.length - windowSize; i++) {
+    const {
+      sum: currentWindowSum,
+      remainingMeasurements: currentRemainingMeasurements,
+    } = getEarliestWindowSumAndRemainingMeasurements(
+      windowSize,
+      remainingMeasurements
+    )
+
+    if (currentWindowSum > previousWindowSum) {
+      timesIncreased++
+    }
+    previousWindowSum = currentWindowSum
+    remainingMeasurements = currentRemainingMeasurements
+  }
+  return timesIncreased
+}
+
+const part1 = (measurements: number[]) => {
+  console.log(getNumberOfIncreasesForWindowSize(measurements, 1))
+}
+
+const part2 = (measurements: number[], windowSize: number) => {
+  console.log(getNumberOfIncreasesForWindowSize(measurements, windowSize))
+}
+
+const input = fs.readFileSync('./inputs/day1/1.txt', 'utf8')
+let measurements: number[] = input
+  .split(/\n/)
+  .map((measurement: string) => parseInt(measurement))
+
+part1(measurements)
+part2(measurements, 3)
