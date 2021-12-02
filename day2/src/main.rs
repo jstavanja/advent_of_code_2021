d4e79544459130f4865965947b2aff0fbd803de7
Jaka Stavanja <jaka.stavanja@oryxgaming.com>
2021-12-02 14:35:55 +0100

    :tada: feat: day 2 - part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,12 +1,19 @@
 use std::fs;
 
-fn main() {
-    let input = fs::read_to_string("../../inputs/day2/1.txt").expect("Unable to read file");
+fn part1(input: &std::string::String) {
+    calculate(input, false);
+}
 
+fn part2(input: &std::string::String) {
+    calculate(input, true);
+}
+
+fn calculate(input: &std::string::String, use_aim: bool) {
     let instructions = input.lines();
 
     let mut depth = 0;
     let mut forward = 0;
+    let mut aim = 0;
     
     for ins in instructions {
         let direction_and_value = ins.split_whitespace().collect::<Vec<_>>();
@@ -16,12 +23,23 @@ fn main() {
         match direction {
             "forward" => {
                 forward = forward + value;
+                if use_aim {
+                    depth = depth + (aim * value);
+                }
             },
             "up" => {
-                depth = depth - value;
+                if use_aim {
+                    aim = aim - value;
+                } else {
+                    depth = depth - value;
+                }
             },
             "down" => {
-                depth = depth + value;
+                if use_aim {
+                    aim = aim + value;
+                } else {
+                    depth = depth + value;
+                }
             },
             _ => {
                 panic!("Unknown action");
@@ -30,4 +48,10 @@ fn main() {
     }
 
     println!("{}", forward*depth);
+}
+
+fn main() {
+    let input = fs::read_to_string("../../inputs/day2/1.txt").expect("Unable to read file");
+    part1(&input);
+    part2(&input);
 }
